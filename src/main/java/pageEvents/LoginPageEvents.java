85f38e8dba6338078b4af0d87938171584a3bc7b
Fabrizio Méndez <fabriziomendezalberti@gmail.com>
2023-10-16 16:06:40 -0600

    checkpoint 8

--- src/main/java/pageEvents/LoginPageEvents.java
+++ src/main/java/pageEvents/LoginPageEvents.java
@@ -1,5 +1,8 @@
 package pageEvents;
 
+import org.testng.Assert;
+
+import pageObject.LoginPageElements;
 import utils.ElementFetch;
 
 public class LoginPageEvents {
@@ -7,5 +10,13 @@ public class LoginPageEvents {
     ElementFetch ele = new ElementFetch();    
     public void verifyIfLoginPageIsLoaded(){
         
+        Assert.assertTrue(ele.getWebElements("XPATH", LoginPageElements.loginText).size()>0, "Element not found");
+
+    }
+
+    public void enterCredentials(){
+        
+        ele.geWebElement("XPATH", LoginPageElements.emailAddress).sendKeys("fabriziomendezalberti@gmail.com");
+        ele.geWebElement("XPATH", LoginPageElements.passwordField).sendKeys("12345");
     }
 }
---
+++ src/test/java/qa/tests/Testcase1.java
@@ -0,0 +1,25 @@
+package qa.tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+import pageEvents.HomePageEvents;
+import pageEvents.LoginPageEvents;
+import utils.ElementFetch;
+
+
+public class Testcase1 extends BaseTest{
+   
+ElementFetch ele = new ElementFetch();
+HomePageEvents homepage = new HomePageEvents();
+LoginPageEvents loginPage = new LoginPageEvents();
+
+    @Test
+    public void sampleMethodForEnteringCredentials(){
+
+        homepage.signInButton();
+        loginPage.verifyIfLoginPageIsLoaded();
+        loginPage.enterCredentials();
+
+    }
+}
