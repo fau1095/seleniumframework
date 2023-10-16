def895791832e3dba207832b0af001161c1df73c
Fabrizio Méndez <fabriziomendezalberti@gmail.com>
2023-10-16 02:36:13 -0600

    sync checkpoint 6

--- src/main/java/pageEvents/LoginPageEvents.java
+++ src/main/java/pageEvents/LoginPageEvents.java
@@ -1,5 +1,11 @@
 package pageEvents;
 
+import utils.ElementFetch;
+
 public class LoginPageEvents {
-    
+
+    ElementFetch ele = new ElementFetch();    
+    public void verifyIfLoginPageIsLoaded(){
+        
+    }
 }
